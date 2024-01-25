08bc5e732f17b750acd0373bf0ee073d1558b8a6
TomiHenriksson <tomihen01@gmail.com>
2024-01-25 13:01:51 +0200

    add updateMediaItem

--- hybrid-graphql-server/src/api/resolvers/mediaResolver.ts
+++ hybrid-graphql-server/src/api/resolvers/mediaResolver.ts
@@ -1,5 +1,5 @@
 import { MediaItem, MediaItemTag } from "@sharedTypes/DBTypes";
-import { fetchAllMedia, fetchMediaById, fetchMediaByTag, postMedia, postTagToMedia } from "../models/mediaModel";
+import { fetchAllMedia, fetchMediaById, fetchMediaByTag, putMedia, postMedia ,postTagToMedia } from "../models/mediaModel";
 
 export default {
   Query: {
@@ -21,6 +21,9 @@ export default {
       const { media_id, tag_id } = args.input;
       const mI = String(media_id)
       return await postTagToMedia(mI, tag_id)
-    }
+    },
+    updateMediaItem: async (_parent: undefined, args: {input: Pick<MediaItem, 'title' | 'description'>; media_id: string }) => {
+      return await putMedia(args.input, Number(args.media_id))
+    },
   },
 }
