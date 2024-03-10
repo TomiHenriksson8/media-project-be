f7b8b87d70b68e26f24f7b630532331b7cbf5089
TomiHenriksson8 <tomihen01@gmail.com>
2024-03-10 15:42:06 +0200

    modify sql query

--- hybrid-media-api-main/src/api/models/mediaModel.ts
+++ hybrid-media-api-main/src/api/models/mediaModel.ts
@@ -99,8 +99,8 @@ const fetchMediaFromFollowedUsers = async (userId: number): Promise<MediaItem[] 
         CONCAT(?, MediaItems.filename) AS filename,
         CONCAT(?, CONCAT(MediaItems.filename, "-thumb.png")) AS thumbnail
       FROM MediaItems
-      JOIN userfollows ON MediaItems.user_id = userfollows.follower_id
-      WHERE userfollows.following_id = ?
+      JOIN userFollows ON MediaItems.user_id = userFollows.follower_id
+      WHERE userFollows.following_id = ?
       ORDER BY MediaItems.created_at DESC`,
       [uploadPath, uploadPath, userId]
     );
