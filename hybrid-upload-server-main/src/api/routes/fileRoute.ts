a8d42a44129f61f22e1e4c5d0506ee3c04888234
TomiHenriksson8 <tomihen01@gmail.com>
2024-03-10 13:32:39 +0200

    path adjustments

--- hybrid-upload-server-main/src/api/routes/fileRoute.ts
+++ hybrid-upload-server-main/src/api/routes/fileRoute.ts
@@ -2,6 +2,7 @@ import express, {Request} from 'express';
 import {deleteFile, uploadFile} from '../controllers/uploadController';
 import multer, {FileFilterCallback} from 'multer';
 import {authenticate, makeThumbnail} from '../../middlewares';
+import path from 'path';
 
 const fileFilter = (
   request: Request,
@@ -14,7 +15,9 @@ const fileFilter = (
     cb(null, false);
   }
 };
-const upload = multer({dest: './uploads/', fileFilter});
+
+const uploadPath = path.join(__dirname, '..', '..', 'uploads');
+const upload = multer({dest: uploadPath, fileFilter});
 const router = express.Router();
 
 // TODO: validation
